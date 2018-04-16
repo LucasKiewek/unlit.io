81b0127b371e5808bf8727f9ab005dc44ea630a6
RichVerySoong <35604166+RichVerySoong@users.noreply.github.com>
2018-04-16 14:42:20 -0400

    Update game.js
    Disabled Browser Zoom

--- client/js/game.js
+++ client/js/game.js
@@ -1,5 +1,27 @@
 function Game() {};
 
+$(document).keydown(function(event) {
+if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+        event.preventDefault();
+     }
+ });
+
+$(window).bind('mousewheel DOMMouseScroll', function (event) {
+       if (event.ctrlKey == true) {
+       event.preventDefault();
+       }
+});
+
+$(document).keydown(function(event) {
+if (event.metaKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+        event.preventDefault();
+     }
+  });
+
+$(window).bind('mousewheel DOMMouseScroll', function (event) {
+       if (event.metaKey == true) {
+       event.preventDefault();
+       }
 
 var PlayArea = class {
     constructor(x, y, width, height, id) {
